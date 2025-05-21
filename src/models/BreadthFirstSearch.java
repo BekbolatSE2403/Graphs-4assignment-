7979dc6812650b42fbb9b7d2b700e1b3651daffb
BekbolatSE2403 <ergalievbek69@gmail.com>
2025-05-21 13:37:18 +0500

    added

---
+++ src/models/BreadthFirstSearch.java
@@ -0,0 +1,29 @@
+package models;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BreadthFirstSearch<V> extends Search<V> {
+    public BreadthFirstSearch(WeightedGraph<V> graph, Vertex<V> source) {
+        super(graph.getSet().stream().filter(v -> v.equals(source)).findFirst().get());
+        bfs(graph, graph.getSet().stream().filter(v -> v.equals(source)).findFirst().get());
+    }
+
+    private void bfs(WeightedGraph<V> graph, Vertex<V> source) {
+        Queue<Vertex<V>> queue = new LinkedList<>();
+        visited.add(source);
+        path.put(source, null);
+        queue.add(source);
+
+        while (!queue.isEmpty()) {
+            Vertex<V> current = queue.poll();
+            for (Vertex<V> neighbor : current.getAdjacentVertices().keySet()) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    path.put(neighbor, current);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+}
